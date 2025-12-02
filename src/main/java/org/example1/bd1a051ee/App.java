7ede92e22134541e385059b0e40ea9aed005fbbf
Eshwar0745 <chindameshwar@gmail.com>
2025-12-02 23:17:48 +0530

    commit

---
+++ src/main/java/org/example1/bd1a051ee/App.java
@@ -0,0 +1,10 @@
+package org.example1.bd1a051ee;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
